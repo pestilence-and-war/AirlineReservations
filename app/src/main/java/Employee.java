d3c6aae0a4fb03febbdcc7cdbb4403dd16ff31fd
Michael Waring <mwaring.mlw@gmail.com>
2023-05-13 20:13:20 -0500

    finished the constructor for Employee

--- app/src/main/java/Employee.java
+++ app/src/main/java/Employee.java
@@ -1,13 +1,24 @@
+import java.util.Date;
+
 public class Employee extends User {
     private int accessLevel;
 
-    // Constructor, getters, and setters
+    // Constructor
+    public Employee(String userId, String firstName, String lastName, String password, Date birthdate, String email, String address, String phoneNumber, int accessLevel) {
+        super(userId, firstName, lastName, password, birthdate, email, address, phoneNumber);
+        this.accessLevel = accessLevel;
+    }
 
-    public int getAccessLevel(){
+    // Default constructor
+    public Employee() {
+    }
+
+    // Getters and setters
+    public int getAccessLevel() {
         return accessLevel;
     }
 
-    public void setAccessLevel(int accessLevel){
+    public void setAccessLevel(int accessLevel) {
         this.accessLevel = accessLevel;
     }
 }
