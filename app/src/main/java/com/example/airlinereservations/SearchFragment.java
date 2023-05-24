06c4783c3d66949a24d1994470b0070f6c79a5f8
Michael Waring <mwaring.mlw@gmail.com>
2023-05-23 22:50:55 -0500

    Added the code for a spinner for origin. still need destination and departure date strings to finish the method call for getting a list of flights.

--- app/src/main/java/com/example/airlinereservations/SearchFragment.java
+++ app/src/main/java/com/example/airlinereservations/SearchFragment.java
@@ -8,10 +8,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.util.List;
+import android.widget.Spinner;
 
 public class SearchFragment extends Fragment {
 
     private SearchViewModel mViewModel;
+
 
     public static SearchFragment newInstance() {
         return new SearchFragment();
@@ -27,7 +30,20 @@ public class SearchFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(SearchViewModel.class);
-        // TODO: Use the ViewModel
+        // actions within the view
+        Spinner spinnerOrigin = view.findViewById(R.id.spinnerOrigin);
 
-    }
+        view.findViewById(R.id.buttonSearch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String origin = spinnerOrigin.getSelectedItem().toString();
+                String destination = "";
+                String departureDate = "";
+                List<Flight> flightSearchResult = FlightSearchActivity.searchFlights(view.getContext(), origin, destination, departureDate);
+            }
+        });
+
+        }
+
+
 }
